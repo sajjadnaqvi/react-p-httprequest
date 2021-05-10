cc505e82e16f7cbe6ac56accd074957678a57b36
Sajjad <sajjadmehdi1214@gmail.com>
2021-05-10 11:25:19 +0500

    httprequest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import axios from "axios";
+import AddTodo from "./Components/AddTodo";
+import TodoItem from "./Components/TodoItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    todos :[]
+  };
+
+  componentDidMount(){
+    axios.get("http://localhost:3333/todos").then((result)=>{
+
+    console.log(result);
+
+    this.setState({
+      todos : result.data
+    });
+    });
+
+  }
+
+  // this function received value from Addtodo component save into the state
+  addTodoToState = (text) =>{
+    axios.post("http://localhost:3333/todos",  {
+      text,//text:text
+      completed:false
+
+    }).then((result)=>{
+      const newTodos = this.state.todos.concat({
+        text: text
+      });
+  
+      this.setState({
+        todos:newTodos
+      });
+    });
+  };
+
+  editTodoFromState = (index,newText)=>{
+    const todo = this.state.todos[index];
+
+    axios.put("http://localhost:3333/todos/" +todo.id,{
+      ...todo,
+      text:newText
+    }).then(()=>{
+      const newTodo = this.state.todos.map((todo,i)=>{
+        if(index === i){
+          return {
+            ...todo,
+            text : newText
+          }
+        }
+        return todo;
+      });
+      this.setState({
+        todos:newTodo
+      });
+    });
+    
+  };
+
+  toggleComplete = (index) =>{
+    const {todos} =this.state;
+    const todo = todos[index];
+    const newTodos = todos.map((todo,i) =>{
+      if(index === i)
+      {
+        return {
+          ...todo,
+          completed : !todo.completed
+        };
+      }
+
+      return todo;
+    } );
+
+    axios.put("http://localhost:3333/todos/"+todo.id,{
+      
+      completed: !todo.completed
+    }).then(()=>{
+      this.setState({
+        todo: newTodos
+      });
+    })
+   
+  }
+
+  deleteTodoFromState = index  =>
+  {
+    const todo = this.state.todos[index];
+    axios.delete("http://localhost:3333/todos/" +todo.id).then(()=>{
+      const newTodos = this.state.todos.filter((todo,i)=>{
+        if(index === i)
+        {
+          return false;
+        }
+        return true;
+      });
+      this.setState({
+        todos:newTodos
+      });
+    });
+  };
+
+  render(){
+    return(
+      <div className="App">
+
+        {this.state.todos.map((todo,index) =>{
+          return (<TodoItem
+           toggleComplete={this.toggleComplete}
+           deleteTodoFromState ={this.deleteTodoFromState}
+           editTodoFromState={this.editTodoFromState}
+           todo={todo}
+           index = {index}
+            key={index}/>
+          );
+        })}
+        <AddTodo addTodoToState={this.addTodoToState}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
